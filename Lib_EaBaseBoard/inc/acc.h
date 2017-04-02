06517db12e5d59d8c3ef0a8167af92c4b36f8948
GitMeGet <tanyupeng1994@gmail.com>
2017-04-02 20:42:02 +0800

    accel level detection interrupt

--- Lib_EaBaseBoard/inc/acc.h
+++ Lib_EaBaseBoard/inc/acc.h
@@ -30,7 +30,7 @@ void acc_init (void);
 void acc_read (int8_t *x, int8_t *y, int8_t *z);
 void acc_setRange(acc_range_t range);
 void acc_setMode(acc_mode_t mode);
-
+void acc_config_mode_LEVEL(void);
 
 
 #endif /* end __LIGHT_H */
--- Lib_EaBaseBoard/src/acc.c
+++ Lib_EaBaseBoard/src/acc.c
@@ -59,11 +59,9 @@
 #define ACC_MCTL_MODE(m) ((m) << 0)
 #define ACC_MCTL_GLVL(g) ((g) << 2)
 
-
 #define ACC_STATUS_DRDY 0x01
 #define ACC_STATUS_DOVR 0x02
 #define ACC_STATUS_PERR 0x04
-
 
 /******************************************************************************
  * External global variables
@@ -73,8 +71,7 @@
  * Local variables
  *****************************************************************************/
 
-static int I2CRead(uint8_t addr, uint8_t* buf, uint32_t len)
-{
+static int I2CRead(uint8_t addr, uint8_t* buf, uint32_t len) {
 	I2C_M_SETUP_Type rxsetup;
 
 	rxsetup.sl_addr7bit = addr;
@@ -84,15 +81,15 @@ static int I2CRead(uint8_t addr, uint8_t* buf, uint32_t len)
 	rxsetup.rx_length = len;
 	rxsetup.retransmissions_max = 3;
 
-	if (I2C_MasterTransferData(I2CDEV, &rxsetup, I2C_TRANSFER_POLLING) == SUCCESS){
+	if (I2C_MasterTransferData(I2CDEV, &rxsetup, I2C_TRANSFER_POLLING)
+			== SUCCESS) {
 		return (0);
 	} else {
 		return (-1);
 	}
 }
 
-static int I2CWrite(uint8_t addr, uint8_t* buf, uint32_t len)
-{
+static int I2CWrite(uint8_t addr, uint8_t* buf, uint32_t len) {
 	I2C_M_SETUP_Type txsetup;
 
 	txsetup.sl_addr7bit = addr;
@@ -102,43 +99,40 @@ static int I2CWrite(uint8_t addr, uint8_t* buf, uint32_t len)
 	txsetup.rx_length = 0;
 	txsetup.retransmissions_max = 3;
 
-	if (I2C_MasterTransferData(I2CDEV, &txsetup, I2C_TRANSFER_POLLING) == SUCCESS){
+	if (I2C_MasterTransferData(I2CDEV, &txsetup, I2C_TRANSFER_POLLING)
+			== SUCCESS) {
 		return (0);
 	} else {
 		return (-1);
 	}
 }
 
+static uint8_t getStatus(void) {
+	uint8_t buf[1];
 
-static uint8_t getStatus(void)
-{
-    uint8_t buf[1];
+	buf[0] = ACC_ADDR_STATUS;
+	I2CWrite(ACC_I2C_ADDR, buf, 1);
+	I2CRead(ACC_I2C_ADDR, buf, 1);
 
-    buf[0] = ACC_ADDR_STATUS;
-    I2CWrite(ACC_I2C_ADDR, buf, 1);
-    I2CRead(ACC_I2C_ADDR, buf, 1);
-
-    return buf[0];
+	return buf[0];
 }
 
-static uint8_t getModeControl(void)
-{
-    uint8_t buf[1];
+static uint8_t getModeControl(void) {
+	uint8_t buf[1];
 
-    buf[0] = ACC_ADDR_MCTL;
-    I2CWrite(ACC_I2C_ADDR, buf, 1);
-    I2CRead(ACC_I2C_ADDR, buf, 1);
+	buf[0] = ACC_ADDR_MCTL;
+	I2CWrite(ACC_I2C_ADDR, buf, 1);
+	I2CRead(ACC_I2C_ADDR, buf, 1);
 
-    return buf[0];
+	return buf[0];
 }
 
-static void setModeControl(uint8_t mctl)
-{
-    uint8_t buf[2];
+static void setModeControl(uint8_t mctl) {
+	uint8_t buf[2];
 
-    buf[0] = ACC_ADDR_MCTL;
-    buf[1] = mctl;
-    I2CWrite(ACC_I2C_ADDR, buf, 2);
+	buf[0] = ACC_ADDR_MCTL;
+	buf[1] = mctl;
+	I2CWrite(ACC_I2C_ADDR, buf, 2);
 }
 
 /******************************************************************************
@@ -155,13 +149,12 @@ static void setModeControl(uint8_t mctl)
  *    Initialize the ISL29003 Device
  *
  *****************************************************************************/
-void acc_init (void)
-{
+void acc_init(void) {
 
-    /* set to measurement mode by default */
+	/* set to measurement mode by default */
 
-    setModeControl( (ACC_MCTL_MODE(ACC_MODE_MEASURE)
-            | ACC_MCTL_GLVL(ACC_RANGE_2G) ));
+	setModeControl(
+			(ACC_MCTL_MODE(ACC_MODE_MEASURE) | ACC_MCTL_GLVL(ACC_RANGE_2G)));
 
 }
 
@@ -176,34 +169,34 @@ void acc_init (void)
  *   [out] z - read z value
  *
  *****************************************************************************/
-void acc_read (int8_t *x, int8_t *y, int8_t *z)
-{
-    uint8_t buf[1];
+void acc_read(int8_t *x, int8_t *y, int8_t *z) {
+	uint8_t buf[1];
 
-    /* wait for ready flag */
-    while ((getStatus() & ACC_STATUS_DRDY) == 0);
+	/* wait for ready flag */
+	while ((getStatus() & ACC_STATUS_DRDY) == 0)
+		;
 
-    /*
-     * Have experienced problems reading all registers
-     * at once. Change to reading them one-by-one.
-     */
-    buf[0] = ACC_ADDR_XOUT8;
-    I2CWrite(ACC_I2C_ADDR, buf, 1);
-    I2CRead(ACC_I2C_ADDR, buf, 1);
+	/*
+	 * Have experienced problems reading all registers
+	 * at once. Change to reading them one-by-one.
+	 */
+	buf[0] = ACC_ADDR_XOUT8;
+	I2CWrite(ACC_I2C_ADDR, buf, 1);
+	I2CRead(ACC_I2C_ADDR, buf, 1);
 
-    *x = (int8_t)buf[0];
+	*x = (int8_t) buf[0];
 
-    buf[0] = ACC_ADDR_YOUT8;
-    I2CWrite(ACC_I2C_ADDR, buf, 1);
-    I2CRead(ACC_I2C_ADDR, buf, 1);
+	buf[0] = ACC_ADDR_YOUT8;
+	I2CWrite(ACC_I2C_ADDR, buf, 1);
+	I2CRead(ACC_I2C_ADDR, buf, 1);
 
-    *y = (int8_t)buf[0];
+	*y = (int8_t) buf[0];
 
-    buf[0] = ACC_ADDR_ZOUT8;
-    I2CWrite(ACC_I2C_ADDR, buf, 1);
-    I2CRead(ACC_I2C_ADDR, buf, 1);
+	buf[0] = ACC_ADDR_ZOUT8;
+	I2CWrite(ACC_I2C_ADDR, buf, 1);
+	I2CRead(ACC_I2C_ADDR, buf, 1);
 
-    *z = (int8_t)buf[0];
+	*z = (int8_t) buf[0];
 }
 
 /******************************************************************************
@@ -215,16 +208,15 @@ void acc_read (int8_t *x, int8_t *y, int8_t *z)
  *   [in] range - the g-Range
  *
  *****************************************************************************/
-void acc_setRange(acc_range_t range)
-{
-    uint8_t mctl = 0;
+void acc_setRange(acc_range_t range) {
+	uint8_t mctl = 0;
 
-    mctl = getModeControl();
+	mctl = getModeControl();
 
-    mctl &= ~(0x03 << 2);
-    mctl |= ACC_MCTL_GLVL(range);
+	mctl &= ~(0x03 << 2);
+	mctl |= ACC_MCTL_GLVL(range);
 
-    setModeControl(mctl);
+	setModeControl(mctl);
 }
 
 /******************************************************************************
@@ -236,15 +228,49 @@ void acc_setRange(acc_range_t range)
  *   [in] mode - the mode to set
  *
  *****************************************************************************/
-void acc_setMode(acc_mode_t mode)
-{
-    uint8_t mctl = 0;
+void acc_setMode(acc_mode_t mode) {
+	uint8_t mctl = 0;
 
-    mctl = getModeControl();
+	mctl = getModeControl();
 
-    mctl &= ~(0x03 << 0);
-    mctl |= ACC_MCTL_MODE(mode);
+	mctl &= ~(0x03 << 0);
+	mctl |= ACC_MCTL_MODE(mode);
 
-    setModeControl(mctl);
+	setModeControl(mctl);
 }
 
+void acc_config_mode_LEVEL(void) {
+	uint8_t buf[2];
+
+	// reg 18 THOPT = 0
+	// reg 18 INTREG[1:0] = 01 (level detection on int pin 2 == PIO1_8)
+	buf[0] = ACC_ADDR_CTL1;
+	I2CWrite(ACC_I2C_ADDR, buf, 1);
+	I2CRead(ACC_I2C_ADDR, buf, 1);
+
+	buf[1] = buf[0] |= 0x02;
+	buf[0] = ACC_ADDR_CTL1;
+	I2CWrite(ACC_I2C_ADDR, buf, 2);
+
+	// reg 19 LDPL = 0 (level detect polarity +ve)
+//	buf[0] = ACC_ADDR_CTL2;
+//	I2CWrite(ACC_I2C_ADDR, buf, 1);
+//	I2CRead(ACC_I2C_ADDR, buf, 1);
+//
+//	buf[1] = buf[0] |= 0x00;
+//	buf[0] = ACC_ADDR_CTL2;
+//	I2CWrite(ACC_I2C_ADDR, buf, 2);
+//
+	// reg 1A LDTH = 2F (threshold)
+	buf[0] = ACC_ADDR_LDTH;
+	buf[1] = 0x07;
+	I2CWrite(ACC_I2C_ADDR, buf, 2);
+
+	buf[0] = ACC_ADDR_INTRST;
+	buf[1] = 0x03;
+	I2CWrite(ACC_I2C_ADDR, buf, 2);
+
+	buf[0] = ACC_ADDR_INTRST;
+	buf[1] = 0x00;
+	I2CWrite(ACC_I2C_ADDR, buf, 2);
+}
